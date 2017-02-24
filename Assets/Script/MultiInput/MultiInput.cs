2eb67a8e162f64508f46d6761e522801c391c4ad
SasakiMizuki <yawarakko@gmail.com>
2017-02-24 22:54:01 +0900

    マルチインプットのデータを更新し、それを読み込む機能を追加。

--- Assets/Script/MultiInput/MultiInput.cs
+++ Assets/Script/MultiInput/MultiInput.cs
@@ -47,12 +47,18 @@ Sensitivity:3
 Type:JoystickAxis
 Axis:6th axis
 
+☆追加機能
+実行中に歯車アイコンのタブを開いて「KeyDataのアタッチメント」を行うとキーデータの上書きができます。
+現在keyData.binの中身が不十分だとバグります、ごめんなさい。解決策模索中....
+
 PS.頭悪いプログラムでごめんなさい☆
 */
 //=========================================
 using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
+using UnityEditor;
 
 public class MultiInput : SingletonMonoBehaviour<MultiInput> {
     public enum CONTROLLER_BUTTON {
@@ -76,11 +82,13 @@ public class MultiInput : SingletonMonoBehaviour<MultiInput> {
         RIGHTSTICK_RIGHT,// 右スティック右 
         LEFTSTICK_UP,    // 左スティック上
         LEFTSTICK_DOWN,  // 左スティック下
-		LEFTSTICK_LEFT,  // 左スティック左
-		LEFTSTICK_RIGHT, // 左スティック右
-		START,			 // 左スティック左
-		SELECT,			 // 左スティック右
-	};
+        LEFTSTICK_LEFT,  // 左スティック左
+        LEFTSTICK_RIGHT, // 左スティック右
+        START,           // 左スティック左
+        SELECT,			 // 左スティック右
+
+        MAX_BUTTOM_NUM,
+    };
 
     public enum STICK_AXIS {
         LEFTSTICK_UP = 0,
@@ -96,7 +104,7 @@ public class MultiInput : SingletonMonoBehaviour<MultiInput> {
     [Header("ボタン名はDUALSHOCK基準です")]
     [Header("各ボタンに合わせたいキーを入れてください。")]
     public string ButtonClossUp;    // 十字キー上
-	public string ButtonClossDown;	// 十字キー下
+    public string ButtonClossDown;	// 十字キー下
     public string ButtonClossLeft;	// 十字キー左
     public string ButtonClossRight;	// 十字キー右
     public string ButtonCircle;		// ○ボタン(Aボタン)
@@ -116,18 +124,141 @@ public class MultiInput : SingletonMonoBehaviour<MultiInput> {
     public string StickL_Up;		// 左スティック上
     public string StickL_Down;		// 左スティック下
     public string StickL_Left;      // 左スティック左
-	public string StickL_Right;     // 左スティック右
-	public string ButtonStart;      // スタート
-	public string ButtonSelect;     // セレクト
+    public string StickL_Right;     // 左スティック右
+    public string ButtonStart;      // スタート
+    public string ButtonSelect;     // セレクト
 
-	private Dictionary<CONTROLLER_BUTTON, string> keyDictionary;
+    [SerializeField]
+    private Dictionary<CONTROLLER_BUTTON, string> keyDictionary;
     private Dictionary<CONTROLLER_BUTTON, KeyCode> padDictionary;
 
     private bool[] BeforeClossButton;
     private bool[] BeforeStick;
     private bool[] NowStick;
+
+    [SerializeField]
+    private string KeyBoardDataFileName = "keyData";
+
+    new void Awake() {
+        ReadingKeyBoardData();    // データ読み込み
+        AttachKeyBoard();
+        // げーむぱっど(Joystick1Button19は対応するボタンがなかったので仮で入れてあるものです)
+        padDictionary = new Dictionary<CONTROLLER_BUTTON, KeyCode>()
+        {
+            {CONTROLLER_BUTTON.CLOSS_UP,   KeyCode.Joystick1Button19},
+            {CONTROLLER_BUTTON.CLOSS_DOWN, KeyCode.Joystick1Button19},
+            {CONTROLLER_BUTTON.CLOSS_LEFT, KeyCode.Joystick1Button19},
+            {CONTROLLER_BUTTON.CLOSS_RIGHT,KeyCode.Joystick1Button19},
+            {CONTROLLER_BUTTON.CIRCLE,     KeyCode.Joystick1Button3},
+            {CONTROLLER_BUTTON.CANCEL,     KeyCode.Joystick1Button2},
+            {CONTROLLER_BUTTON.TRIANGLE,   KeyCode.Joystick1Button1},
+            {CONTROLLER_BUTTON.SQUARE,     KeyCode.Joystick1Button0},
+            {CONTROLLER_BUTTON.RIGHT_1,    KeyCode.Joystick1Button5},
+            {CONTROLLER_BUTTON.RIGHT_2,    KeyCode.Joystick1Button7},
+            {CONTROLLER_BUTTON.RIGHT_3,    KeyCode.Joystick1Button12},
+            {CONTROLLER_BUTTON.LEFT_1,     KeyCode.Joystick1Button4},
+            {CONTROLLER_BUTTON.LEFT_2,     KeyCode.Joystick1Button6},
+            {CONTROLLER_BUTTON.LEFT_3,     KeyCode.Joystick1Button11},
+            {CONTROLLER_BUTTON.RIGHTSTICK_UP,      KeyCode.Joystick1Button19},
+            {CONTROLLER_BUTTON.RIGHTSTICK_DOWN,    KeyCode.Joystick1Button19},
+            {CONTROLLER_BUTTON.RIGHTSTICK_LEFT,    KeyCode.Joystick1Button19},
+            {CONTROLLER_BUTTON.RIGHTSTICK_RIGHT,   KeyCode.Joystick1Button19},
+            {CONTROLLER_BUTTON.LEFTSTICK_UP,       KeyCode.Joystick1Button19},
+            {CONTROLLER_BUTTON.LEFTSTICK_DOWN,     KeyCode.Joystick1Button19},
+            {CONTROLLER_BUTTON.LEFTSTICK_LEFT,     KeyCode.Joystick1Button19},
+            {CONTROLLER_BUTTON.LEFTSTICK_RIGHT,    KeyCode.Joystick1Button19},
+            {CONTROLLER_BUTTON.START,              KeyCode.Joystick1Button10},
+            {CONTROLLER_BUTTON.SELECT,             KeyCode.Joystick1Button9},
+        };
+
+        BeforeClossButton = new bool[4];
+        BeforeStick = new bool[8];  // 0を左スティックの上方向として時計回りに1,2,3。4以降は右スティックで同じく時計回りに5,6,7。
+        NowStick = new bool[8];
+    }
+
     // Use this for initialization
-    void Start () {
+    void Start() {
+    }
+
+    // Update is called once per frame   
+    void Update() {
+        NowStick[0] = GetLeftStickAxis().y > 0.8f ? true : false;
+        NowStick[1] = GetLeftStickAxis().x > 0.8f ? true : false;
+        NowStick[2] = GetLeftStickAxis().y < -0.8f ? true : false;
+        NowStick[3] = GetLeftStickAxis().x < -0.8f ? true : false;
+        NowStick[4] = GetRightStickAxis().y > 0.8f ? true : false;
+        NowStick[5] = GetRightStickAxis().x > 0.8f ? true : false;
+        NowStick[6] = GetRightStickAxis().y < -0.8f ? true : false;
+        NowStick[7] = GetRightStickAxis().x < -0.8f ? true : false;
+    }
+
+    void LateUpdate() {
+        int i;
+        // 現在フレームで入力されているかの確認(TriggerとRelease用)
+        for(i = 0; i < BeforeClossButton.Length; i++) {
+            BeforeClossButton[i] = GetPressButton((CONTROLLER_BUTTON)i);
+        }
+        for(i = 0; i < 8; i++) {
+            BeforeStick[i] = NowStick[i];
+        }
+    }
+
+    /// <summary>
+    /// キーボードデータの取得
+    /// </summary>
+    private void ReadingKeyBoardData() {
+        FileInfo keyDataFile = new FileInfo("Data/" + KeyBoardDataFileName + ".bin");
+        if(keyDataFile == null) {
+            return;
+        }
+        StreamReader streamReader = new StreamReader(keyDataFile.OpenRead());
+        // キーの割り当て
+        ButtonClossUp = streamReader.ReadLine();
+        ButtonClossDown = streamReader.ReadLine();
+        ButtonClossLeft = streamReader.ReadLine();
+        ButtonClossRight = streamReader.ReadLine();
+        ButtonCircle = streamReader.ReadLine();
+        ButtonCancel = streamReader.ReadLine();
+        ButtonTriangle = streamReader.ReadLine();
+        ButtonSquare = streamReader.ReadLine();
+        ButtonR1 = streamReader.ReadLine();
+        ButtonR2 = streamReader.ReadLine();
+        ButtonR3 = streamReader.ReadLine();
+        ButtonL1 = streamReader.ReadLine();
+        ButtonL2 = streamReader.ReadLine();
+        ButtonL3 = streamReader.ReadLine();
+        StickR_Up = streamReader.ReadLine();
+        StickR_Down = streamReader.ReadLine();
+        StickR_Left = streamReader.ReadLine();
+        StickR_Right = streamReader.ReadLine();
+        StickL_Up = streamReader.ReadLine();
+        StickL_Down = streamReader.ReadLine();
+        StickL_Left = streamReader.ReadLine();
+        StickL_Right = streamReader.ReadLine();
+        ButtonStart = streamReader.ReadLine();
+        ButtonSelect = streamReader.ReadLine();
+        streamReader.Close();
+    }
+
+    /// <summary>
+    /// キーボードデータの書き出し
+    /// </summary>
+    private void OutputKeyBoardData() {
+        FileInfo keyDataFile = new FileInfo("Data/" + KeyBoardDataFileName + ".bin");
+        StreamWriter streamWriter;
+        streamWriter = keyDataFile.CreateText();
+        int i;
+        for(i = 0; i < (int)CONTROLLER_BUTTON.MAX_BUTTOM_NUM; i++) {
+            streamWriter.WriteLine(keyDictionary[(CONTROLLER_BUTTON)i]);
+        }
+        streamWriter.Flush();
+        streamWriter.Close();
+    }
+
+    /// <summary>
+    /// キーボードの読み込み、更新
+    /// </summary>
+    private void UpdateKeyData() {
         // きーぼーど
         keyDictionary = new Dictionary<CONTROLLER_BUTTON, string>()
         {
@@ -151,71 +282,24 @@ public class MultiInput : SingletonMonoBehaviour<MultiInput> {
             {CONTROLLER_BUTTON.RIGHTSTICK_RIGHT,   StickR_Right},
             {CONTROLLER_BUTTON.LEFTSTICK_UP,       StickL_Up},
             {CONTROLLER_BUTTON.LEFTSTICK_DOWN,     StickL_Down},
-			{CONTROLLER_BUTTON.LEFTSTICK_LEFT,     StickL_Left},
-			{CONTROLLER_BUTTON.LEFTSTICK_RIGHT,    StickL_Right},
-			{CONTROLLER_BUTTON.START,			   ButtonStart},
-			{CONTROLLER_BUTTON.SELECT,			   ButtonSelect},
-		};
-
-		// げーむぱっど(Joystick1Button19は対応するボタンがなかったので仮で入れてあるものです)
-		padDictionary = new Dictionary<CONTROLLER_BUTTON, KeyCode>()
-        {
-            {CONTROLLER_BUTTON.CLOSS_UP,   KeyCode.Joystick1Button19},
-            {CONTROLLER_BUTTON.CLOSS_DOWN, KeyCode.Joystick1Button19},
-            {CONTROLLER_BUTTON.CLOSS_LEFT, KeyCode.Joystick1Button19},
-            {CONTROLLER_BUTTON.CLOSS_RIGHT,KeyCode.Joystick1Button19},
-            {CONTROLLER_BUTTON.CIRCLE,     KeyCode.Joystick1Button3},
-            {CONTROLLER_BUTTON.CANCEL,     KeyCode.Joystick1Button2},
-            {CONTROLLER_BUTTON.TRIANGLE,   KeyCode.Joystick1Button1},
-            {CONTROLLER_BUTTON.SQUARE,     KeyCode.Joystick1Button0},
-            {CONTROLLER_BUTTON.RIGHT_1,    KeyCode.Joystick1Button5},
-            {CONTROLLER_BUTTON.RIGHT_2,    KeyCode.Joystick1Button7},
-            {CONTROLLER_BUTTON.RIGHT_3,    KeyCode.Joystick1Button12},
-            {CONTROLLER_BUTTON.LEFT_1,     KeyCode.Joystick1Button4},
-            {CONTROLLER_BUTTON.LEFT_2,     KeyCode.Joystick1Button6},
-            {CONTROLLER_BUTTON.LEFT_3,     KeyCode.Joystick1Button11},
-            {CONTROLLER_BUTTON.RIGHTSTICK_UP,      KeyCode.Joystick1Button19},
-            {CONTROLLER_BUTTON.RIGHTSTICK_DOWN,    KeyCode.Joystick1Button19},
-            {CONTROLLER_BUTTON.RIGHTSTICK_LEFT,    KeyCode.Joystick1Button19},
-            {CONTROLLER_BUTTON.RIGHTSTICK_RIGHT,   KeyCode.Joystick1Button19},
-            {CONTROLLER_BUTTON.LEFTSTICK_UP,       KeyCode.Joystick1Button19},
-            {CONTROLLER_BUTTON.LEFTSTICK_DOWN,     KeyCode.Joystick1Button19},
-			{CONTROLLER_BUTTON.LEFTSTICK_LEFT,     KeyCode.Joystick1Button19},
-			{CONTROLLER_BUTTON.LEFTSTICK_RIGHT,    KeyCode.Joystick1Button19},
-			{CONTROLLER_BUTTON.START,			   KeyCode.Joystick1Button10},
-			{CONTROLLER_BUTTON.SELECT,			   KeyCode.Joystick1Button9},
-		};
-
-        BeforeClossButton = new bool[4];
-        BeforeStick = new bool[8];  // 0を左スティックの上方向として時計回りに1,2,3。4以降は右スティックで同じく時計回りに5,6,7。
-        NowStick = new bool[8];
-    }                                              
-	                                               
-	// Update is called once per frame   
-    void Update() {
-        NowStick[0] = GetLeftStickAxis().y > 0.8f ? true : false;
-        NowStick[1] = GetLeftStickAxis().x > 0.8f ? true : false;
-        NowStick[2] = GetLeftStickAxis().y < -0.8f ? true : false;
-        NowStick[3] = GetLeftStickAxis().x < -0.8f ? true : false;
-        NowStick[4] = GetRightStickAxis().y > 0.8f ? true : false;
-        NowStick[5] = GetRightStickAxis().x > 0.8f ? true : false;
-        NowStick[6] = GetRightStickAxis().y < -0.8f ? true : false;
-        NowStick[7] = GetRightStickAxis().x < -0.8f ? true : false;
-    }        
-                  
-	void LateUpdate () {
-        int i;
-		// 現在フレームで入力されているかの確認(TriggerとRelease用)
-        for(i = 0; i < BeforeClossButton.Length; i++) {
-            BeforeClossButton[i] = GetPressButton((CONTROLLER_BUTTON)i);
-        }
-        for(i = 0; i < 8; i++) {
-            BeforeStick[i] = NowStick[i];
-        }
+            {CONTROLLER_BUTTON.LEFTSTICK_LEFT,     StickL_Left},
+            {CONTROLLER_BUTTON.LEFTSTICK_RIGHT,    StickL_Right},
+            {CONTROLLER_BUTTON.START,              ButtonStart},
+            {CONTROLLER_BUTTON.SELECT,             ButtonSelect},
+        };
     }
 
-	// 以下コメントなし。
-			
+    /// <summary>
+    /// 実行中に変更を掛けたキーボードデータのアタッチメント
+    /// </summary>
+    [ContextMenu("KeyDataのアタッチメント")]
+    private void AttachKeyBoard() {
+        UpdateKeyData();
+        AttachKeyBoard();
+    }
+
+    // 以下コメントなし。
+
     public bool GetPressButton(CONTROLLER_BUTTON button) {
         if(button > CONTROLLER_BUTTON.CLOSS_RIGHT) {
             if(Input.GetKey(keyDictionary[button]) || Input.GetKey(padDictionary[button])) {
@@ -230,7 +314,7 @@ public class MultiInput : SingletonMonoBehaviour<MultiInput> {
                 if(Input.GetKey(keyDictionary[button]) || Input.GetAxisRaw("GamePadVertical") == -1.0f) {
                     return true;
                 }
-            }else if(button == CONTROLLER_BUTTON.CLOSS_RIGHT) {
+            } else if(button == CONTROLLER_BUTTON.CLOSS_RIGHT) {
                 if(Input.GetKey(keyDictionary[button]) || Input.GetAxisRaw("GamePadHorizontal") == 1.0f) {
                     return true;
                 }
@@ -285,22 +369,22 @@ public class MultiInput : SingletonMonoBehaviour<MultiInput> {
             }
         } else {
             if(button == CONTROLLER_BUTTON.CLOSS_UP) {
-                if(!Input.GetKey(keyDictionary[button]) && !(Input.GetAxisRaw("GamePadVertical") == 1.0f)) {
+                if(!Input.GetKey(keyDictionary[button]) && !( Input.GetAxisRaw("GamePadVertical") == 1.0f )) {
                     if(BeforeClossButton[0])
                         return true;
                 }
             } else if(button == CONTROLLER_BUTTON.CLOSS_DOWN) {
-                if(!Input.GetKey(keyDictionary[button]) && !(Input.GetAxisRaw("GamePadVertical") == -1.0f)) {
+                if(!Input.GetKey(keyDictionary[button]) && !( Input.GetAxisRaw("GamePadVertical") == -1.0f )) {
                     if(BeforeClossButton[1])
                         return true;
                 }
             } else if(button == CONTROLLER_BUTTON.CLOSS_RIGHT) {
-                if(!Input.GetKey(keyDictionary[button]) && !(Input.GetAxisRaw("GamePadHorizontal") == 1.0f)) {
+                if(!Input.GetKey(keyDictionary[button]) && !( Input.GetAxisRaw("GamePadHorizontal") == 1.0f )) {
                     if(BeforeClossButton[3])
                         return true;
                 }
             } else {
-                if(!Input.GetKey(keyDictionary[button]) && !(Input.GetAxisRaw("GamePadHorizontal") == -1.0f)) {
+                if(!Input.GetKey(keyDictionary[button]) && !( Input.GetAxisRaw("GamePadHorizontal") == -1.0f )) {
                     if(BeforeClossButton[2])
                         return true;
                 }
